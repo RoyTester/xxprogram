f5e3c089195eb08c060d365b378085d8f3994b44
zhonlaoda <zhonlaoda@github.com>
2018-11-05 17:10:00 +0800

    v1.0.0

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='xxprogram',
+    version='1.0.0',
+    py_modules=['XXProgram'],
+    author='ZRui',
+    description='用程序员梗随机生成用户名',
+    author_email='a1571093237@msn.cn',
+    url='https://github.com/zhonlaoda/xxprogram'
+)
